73fb8a0c2469b2a3a351f14460bc86e09e3f5dd4
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-06-05 21:47:52 +0530

    chor:device support status

--- lib/providers/torch_provider.dart
+++ lib/providers/torch_provider.dart
@@ -38,11 +38,49 @@ class TorchNotifier extends Notifier<TorchModel> {
     final isTorchOn = state.flashLightState == FlashLightState.on;
 
     if (isTorchOn) {
-      await TorchLight.disableTorch();
-      state = state.copyWith(flashLightState: FlashLightState.off);
+      try {
+        await TorchLight.disableTorch();
+        state = state.copyWith(flashLightState: FlashLightState.off);
+      } on DisableTorchExistentUserException catch (e) {
+        // The camera is in use
+        state = state.copyWith(
+          errorMessage:
+              'Flashlight is currently in use by another application.',
+        );
+      } on DisableTorchNotAvailableException catch (e) {
+        // Torch was not detected
+        state = state.copyWith(
+          errorMessage: 'Flashlight is not available on this device.',
+        );
+      } on DisableTorchException catch (e) {
+        // Torch could not be disabled due to another error
+        state = state.copyWith(
+          errorMessage:
+              'An error occurred while trying to disable the flashlight.',
+        );
+      }
     } else {
-      await TorchLight.enableTorch();
-      state = state.copyWith(flashLightState: FlashLightState.on);
+      try {
+        await TorchLight.enableTorch();
+        state = state.copyWith(flashLightState: FlashLightState.on);
+      } on EnableTorchExistentUserException catch (e) {
+        // The camera is in use
+        state = state.copyWith(
+          errorMessage:
+              'Flashlight is currently in use by another application.',
+        );
+      } on EnableTorchNotAvailableException catch (e) {
+        // Torch was not detected
+        state = state.copyWith(
+          errorMessage: 'Flashlight is not available on this device.',
+        );
+      } on EnableTorchException catch (e) {
+        // Torch could not be enabled due to another error
+        state = state.copyWith(
+          errorMessage:
+              'An error occurred while trying to enable the flashlight.',
+        );
+      }
     }
   }
 
