06c1c20b706eeb2c3704a2b04ba3dfae9d4aae86
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-06-05 23:48:01 +0530

    chor: icon and data configured

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,12 +21,12 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tourch_app"
+        applicationId = "com.sachin96boy.Tourchapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
     }
 
